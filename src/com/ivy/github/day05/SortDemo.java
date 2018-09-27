d29e748e0d236cf3ebd26b56835feb2e740f6114
ivyzh <app_do@163.com>
2018-09-27 18:36:12 +0800

    冒泡排序

---
+++ src/com/ivy/github/day05/SortDemo.java
@@ -0,0 +1,23 @@
+package com.ivy.github.day05;
+
+// 排序Demo
+
+//排序的分类：
+//选择排序（直接选择排序、堆排序）
+//交换排序(冒泡排序、快速排序)
+//插入排序（直接插入排序、二分法插入排序、Shell排序）
+//归并排序等。
+//排序有升序排列和降序排列之分，我们现在单讲升序排列：
+//我们主要讲解冒泡，选择，插入排序，当然在开发中因为性能问题，我们都不会自己写排序算法，不过排序在笔试题里却是常客。
+
+public class SortDemo {
+
+	public static void main(String[] args) {
+		test01();//冒泡排序
+	}
+
+	private static void test01() {
+		int[] arr = { 2, 9, 6, 7, 4, 1 };
+	}
+
+}
