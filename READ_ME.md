797a6a0707e0d3ac2c30bccc6db7075fd1db7f7c
ivyzh <app_do@163.com>
2018-09-27 11:50:49 +0800

    数据类型转换

---
+++ READ_ME.md
@@ -0,0 +1,8 @@
+### 0. 提交Github地址
+
+> https://github.com/IvyZh/JavaSE_Basic.git
+
+
+### 1. 小白使用eclipse提交到GitHub (详细步骤)
+
+> https://blog.csdn.net/bendanany/article/details/78891804
---
+++ src/com/ivy/github/day02/DataTypeTransDemo.java
@@ -0,0 +1,12 @@
+package com.ivy.github.day02;
+
+//  数据类型转换
+
+public class DataTypeTransDemo {
+
+	public static void main(String[] args) {
+
+		// 1. 自动类型转换，也称为“隐式类型转换:”，小转大
+	}
+
+}
