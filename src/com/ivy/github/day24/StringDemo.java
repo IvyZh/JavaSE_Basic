0a26ad89032fffec8fb4b4c64c64627d2eec4217
ivyzh <app_do@163.com>
2018-10-09 20:17:37 +0800

    finish day24

---
+++ src/com/ivy/github/day24/StringDemo.java
@@ -0,0 +1,11 @@
+package com.ivy.github.day24;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+
+		String ret = String.join("-", "a", "c", "d");
+		System.out.println(ret);
+	}
+
+}
